1af2fef26d369654bb98707b9b66f6f6f0b7aa9c
JULIUS KIRIMA <juliuskirima1@gmail.com>
2026-07-26 17:16:14 +0300

    Add function to determine days in each month

--- script.js
+++ script.js
@@ -51,6 +51,16 @@ birthdate.max = today.toISOString().split("T")[0];
 //Leap Year Function
 
 function isLeapYear (year) {
-  return (year % 400 === 0  || year % 4 === 0 & year % 100 !== 0)
+  return (year % 400 === 0  || year % 4 === 0 && year % 100 !== 0)
 }
 
+//Days in Month
+
+function getDaysInMonth (month, year) {
+  const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  if (month === 2 && isLeapYear(year)) {
+    return 29
+  }
+
+  return days[month - 1]
+}
