ac9d9f82a4e02d43307b14f9fc9da194adf1b221
JULIUS KIRIMA <juliuskirima1@gmail.com>
2026-07-26 15:50:35 +0300

    Retrieve required DOM elements

---
+++ script.js
@@ -0,0 +1,11 @@
+//Dom Element
+const form = document.getElementById("akanForm");
+const birthdate = document.getElementById("birthdate");
+const clearBtn = document.getElementById("clearBtn");
+
+const result = document.getElementById("resultContainer");
+const akanName = document.getElementById("akanNameDisplay");
+const dayText = document.getElementById("dayDisplay");
+
+const dateError = document.getElementById("dateError");
+const genderError = document.getElementById("genderError");
