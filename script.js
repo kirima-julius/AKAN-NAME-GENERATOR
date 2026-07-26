152c96b088afa63a031fe208079c9372feb9dcc6
JULIUS KIRIMA <juliuskirima1@gmail.com>
2026-07-26 15:51:40 +0300

    Add arrays for weekdays and Akan names

--- script.js
+++ script.js
@@ -9,3 +9,35 @@ const dayText = document.getElementById("dayDisplay");
 
 const dateError = document.getElementById("dateError");
 const genderError = document.getElementById("genderError");
+
+
+// ARRAYS
+const weekDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+const maleNames = [
+  "Kwasi",
+  "Kwadwo",
+  "Kwabena",
+  "Kwaku",
+  "Yaw",
+  "Kofi",
+  "Kwame"
+];
+
+const femaleNames = [
+  "Akosua",
+  "Adwoa",
+  "Abenaa",
+  "Akua",
+  "Yaa",
+  "Afua",
+  "Ama"
+];
